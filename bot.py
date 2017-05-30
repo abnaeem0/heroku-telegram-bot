18cef18e515eccd28df6919ca3a6162b2bf27475
abnaeem0 <abubakarnaeem0@gmail.com>
2017-05-30 12:59:13 +0500

    Update bot.py
    Swapped in the webhooks api

--- bot.py
+++ bot.py
@@ -20,6 +20,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import os
 TOKEN = os.environ['TELEGRAM_TOKEN']
+PORT = int(os.environ.get('PORT', '5000'))
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -62,8 +63,10 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
-
+    updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+    updater.bot.set_webhook("https://python-telegram-webhook-jamaat.herokuapp.com/" + TOKEN)
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
